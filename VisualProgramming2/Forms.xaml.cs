1f712232b85f6956426e5a79e1c15459ffe97be9
DevSMike <likashev.migha@yandex.ru>
2022-10-09 23:04:33 +0300

    Удаление ненужных using.

--- VisualProgramming2/Forms.xaml.cs
+++ VisualProgramming2/Forms.xaml.cs
@@ -1,22 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VisualProgramming2
 {
     /// <summary>
-    /// Логика взаимодействия для Forms.xaml
+    /// Логика взаимодействия для виджета Анкеты
     /// </summary>
     public partial class Forms : UserControl
     {
